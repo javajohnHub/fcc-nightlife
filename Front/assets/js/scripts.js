aca9a68b61691f65041d95f5a4320e25c6156f55
Slava Knyazev <slava@knyz.org>
2017-05-08 13:57:32 -0400

    Starting to work

--- Front/assets/js/scripts.js
+++ Front/assets/js/scripts.js
@@ -2,11 +2,24 @@ $( document ).ready(function() {
 
     $.get("https://ipinfo.io", function(response) {
         $.get( "/api/search/" + response.city, function( data ) {
-            $( ".result" ).append(
-                "<li class='list-group-item'>" + data.name + "<img src='" + data.image + "'>" + data.isGoing + data.goingCount + "</li>"
-            );
+            JSON.parse(data).forEach(function (bar){
+                addItem(bar.id, bar.name, bar.image, bar.goingCount, bar.isGoing);
+            })
         });
     }, "jsonp");
 
 });
 
+/**
+ * Add item to results
+ * @param {String} id unique ID associated to the bar
+ * @param {String} name Human-Readable name of bar
+ * @param {String} image URI of image associated with the bar
+ * @param {Number} goingCount Number of people going
+ * @param {Boolean} isGoing If the current (logged-in) user is going
+ */
+function addItem(id, name, image, goingCount, isGoing){
+   $( ".result" ).append(
+                "<li class='list-group-item'>" + name + "<img src='" + image + "'>" + isGoing + goingCount + "</li>"
+            );
+}
