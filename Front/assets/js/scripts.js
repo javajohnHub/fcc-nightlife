b5a8745059c0734396a95db595cd8c8da349e72a
javajohnhub <joedw1978@gmail.com>
2017-05-08 13:31:37 -0400

    created li elements with data
    small change

--- Front/assets/js/scripts.js
+++ Front/assets/js/scripts.js
@@ -2,7 +2,12 @@ $( document ).ready(function() {
 
     $.get("https://ipinfo.io", function(response) {
         $.get( "/api/search/" + response.city, function( data ) {
-            $( ".result" ).html( data );
+            $( ".result" ).html( data, function(){
+                $("#messages").append(
+                    "<li class='list-group-item'>" + data.name + "<img src='" + data.image + "' target='_blank'>" + data.isGoing + "</li>"
+                );
+
+            } );
         });
     }, "jsonp");
 
