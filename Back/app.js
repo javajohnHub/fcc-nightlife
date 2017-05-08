a21654be90de38d2a52a6f771304e47a3064b55c
javajohnhub <joedw1978@gmail.com>
2017-05-08 12:43:15 -0400

    hghsgfh

--- Back/app.js
+++ Back/app.js
@@ -20,13 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-<<<<<<< HEAD
-=======
+
 app.use(express.static(path.join(__dirname, 'public')));
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
 app.use(session({ secret: 'keyboard cat' }))
->>>>>>> origin/master
+
 
 app.use('/api', api);
 app.use('/', ui);
