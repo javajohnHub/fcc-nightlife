095ef081613aa97ad590a62ce7db57602e698283
javajohnhub <joedw1978@gmail.com>
2017-05-08 14:33:54 -0400

    added button click

--- Front/assets/js/scripts.js
+++ Front/assets/js/scripts.js
@@ -23,3 +23,20 @@ function addItem(id, name, image, goingCount, isGoing){
                 "<li class='list-group-item'>" + "<div class='row'><img class='img-fluid img-thumbnail rounded float-left' style='width:100px;height:100px;' src='" + image + "'>" + "<h3 class='text-center'>" + name + "</h3>" + "<div class='col'>I Going " + isGoing + "</div>" + "<div class='col'>Going count " + goingCount + "</div></div></li>"
             );
 }
+
+function replaceItem(id, name, image, goingCount, isGoing){
+    $(".result").innerHTML = '';
+    $( ".result" ).append(
+        "<li class='list-group-item'>" + "<div class='row'><img class='img-fluid img-thumbnail rounded float-left' style='width:100px;height:100px;' src='" + image + "'>" + "<h3 class='text-center'>" + name + "</h3>" + "<div class='col'>I Going " + isGoing + "</div>" + "<div class='col'>Going count " + goingCount + "</div></div></li>"
+    );
+}
+$("#go").on('click', function(){
+    $.get("https://ipinfo.io", function(response) {
+        $.get( "/api/search/" + $("#city").val(), function( data ) {
+            JSON.parse(data).forEach(function (bar){
+                replaceItem(bar.id, bar.name, bar.image, bar.goingCount, bar.isGoing);
+            })
+        });
+    }, "jsonp");
+
+});
