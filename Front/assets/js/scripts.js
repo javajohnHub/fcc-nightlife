bd3509eab108f7e07dae255964e7cc11e4a413a1
javajohnhub <joedw1978@gmail.com>
2017-05-08 13:54:41 -0400

    is this correct?

--- Front/assets/js/scripts.js
+++ Front/assets/js/scripts.js
@@ -2,12 +2,9 @@ $( document ).ready(function() {
 
     $.get("https://ipinfo.io", function(response) {
         $.get( "/api/search/" + response.city, function( data ) {
-            $( ".result" ).html( data, function(){
-                $("#messages").append(
-                    "<li class='list-group-item'>" + data.name + "<img src='" + data.image + "'>" + data.isGoing + data.goingCount + "</li>"
-                );
-
-            } );
+            $( ".result" ).append(
+                "<li class='list-group-item'>" + data.name + "<img src='" + data.image + "'>" + data.isGoing + data.goingCount + "</li>"
+            );
         });
     }, "jsonp");
 
